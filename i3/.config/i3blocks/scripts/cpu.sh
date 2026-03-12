bc472c89e91c4519e0485f0d1276f464cbafdd18
Abr-ahamis <87413272+Abr-ahamis@users.noreply.github.com>
2026-03-12 02:51:24 -0700

    Update cpu.sh

--- i3/.config/i3blocks/scripts/cpu.sh
+++ i3/.config/i3blocks/scripts/cpu.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+# i3blocks CPU/Mem/Swap using top as the data source (non-interactive)
+set -u
 
-read cpu user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat
-prev_idle=$((idle + iowait))
-prev_total=$((user + nice + system + idle + iowait + irq + softirq + steal))
+top_out=$(top -bn1)
 
-sleep 0.1
+cpu_usage=$(awk -F'[:, ]+' '/^%Cpu\(s\):/ { for (i=1;i<=NF;i++) if ($i=="id") { id=$(i-1); } }
+  END { if (id=="") { print "0"; } else { printf "%3.0f", 100 - id; } }' <<<"$top_out")
 
-read cpu user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat
-idle_now=$((idle + iowait))
-total_now=$((user + nice + system + idle + iowait + irq + softirq + steal))
+mem_line=$(awk -F'[:, ]+' '/^MiB Mem/ {print $0}' <<<"$top_out")
+swap_line=$(awk -F'[:, ]+' '/^MiB Swap/ {print $0}' <<<"$top_out")
 
-diff_total=$((total_now - prev_total))
-diff_idle=$((idle_now - prev_idle))
+mem_used=$(awk -F'[, ]+' '{ for (i=1;i<=NF;i++) if ($i=="used") { print $(i-1); exit } }' <<<"$mem_line")
+mem_total=$(awk -F'[, ]+' '{ for (i=1;i<=NF;i++) if ($i=="total") { print $(i-1); exit } }' <<<"$mem_line")
 
-cpu_usage=$(awk -v idle="$diff_idle" -v total="$diff_total" \
-  'BEGIN { printf "%3.0f", 100 * (1 - idle/total) }')
+swap_used=$(awk -F'[, ]+' '{ for (i=1;i<=NF;i++) if ($i=="used") { print $(i-1); exit } }' <<<"$swap_line")
+swap_total=$(awk -F'[, ]+' '{ for (i=1;i<=NF;i++) if ($i=="total") { print $(i-1); exit } }' <<<"$swap_line")
 
-echo " CPU ${cpu_usage}%"
-                           
+# Fallbacks if parsing fails
+mem_used=${mem_used:-0}
+mem_total=${mem_total:-0}
+swap_used=${swap_used:-0}
+swap_total=${swap_total:-0}
+
+echo " CPU ${cpu_usage}%  |   ${mem_used}% / ${mem_total}% MiB  |    ${swap_used}% / ${swap_total} MiB"
